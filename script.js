bdbe6178a7e5c07ec4967b294dd67ee5e7164321
agentginger <coperu932@gmail.com>
2024-04-20 15:54:17 +0100

    Linked buttons to functions,

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore() {
+  console.log("Going to store.")
+  button1.innerText = "buy 10 health (10 gold)"
+};
+
+function goCave() {
+  console.log("Going to cave.")
+}
+
+function fightDragon() {
+  console.log("Fighting Dragon.")
+}
+
+// initialize buttons
+
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
